ef5e68c95b2ebb92c4b27eba542e4057c939911e
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-03 16:49:32 -0500

    Custom Theme

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'recipe_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = RecipeTheme.light();
+    // TODO: Apply Home widget
+    // 3
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        appBar: AppBar(title: Text('Fooderlich', style: theme.textTheme.headline6,)),
+        body: Center(child: Text('Let\'s get cooking 👩‍🍳', style: theme.textTheme.headline2,)),
+      ),
+    );
+  }
+}
