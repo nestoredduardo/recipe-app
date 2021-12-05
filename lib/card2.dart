ffe7d84f53a83db4bca73969a98deeaff9a8b34d
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-05 11:25:32 -0500

    Author Card params

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'components/author_card.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -23,8 +24,12 @@ class Card2 extends StatelessWidget {
         ),
         // 2
         child: Column(
-          children: [
-            // TODO 1: add author information
+          children: const [
+            AuthorCard(
+              authorName: 'Mike Katz',
+              title: 'Smoothie Connoisseur',
+              imageProvider: AssetImage('assets/author_katz.jpeg'),
+            ),
             // TODO 4: add Positioned text
           ],
         ),
